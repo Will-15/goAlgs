2dd4fe59df6bced456078dcb7182152e8a44c8b5
Will-15 <185955984+Will-15@users.noreply.github.com>
2024-10-30 00:00:00 +0500

    'add file dfs.go'

---
+++ dfs.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var visited []bool
+
+func (g *Graph) DFS(start int) {
+	visited := make([]bool, g.vertices)
+	g.dfsUtil(start, visited)
+	fmt.Println()
+}
+
+func (g *Graph) dfsUtil(node int, visited []bool) {
+	visited[node] = true
+	fmt.Print(node, " ")
+
+	for _, neighbor := range g.adjList[node] {
+		if !visited[neighbor] {
+			g.dfsUtil(neighbor, visited)
+		}
+	}
+}
